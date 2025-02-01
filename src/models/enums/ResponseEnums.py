0e642432001459c64cb72f8add6c7f23053ae326
SalwaMustafa <salwa2532003@gmail.com>
2025-02-01 08:12:01 +0200

    Add validation messages

--- src/models/enums/ResponseEnums.py
+++ src/models/enums/ResponseEnums.py
@@ -2,8 +2,11 @@ from enum import Enum
 
 class ResponseSignal(Enum):
 
+    FILE_VALIDATED_SUCCESS = "File_validate_successfuly"
     FILE_TYPE_NOT_SUPPORTED = "File type not supported"
     FILE_SIZE_EXCEEDED = "File size exceeded"
     FILE_UPLOADED_SUCCESS = "File uploaded success"
     FILE_UPLOADED_FAILED = "File uploaded failed"
+    PROCESSING_FAILED = "Processing_failed"
+    PROCESSING_SUCCESS = "Processing_success"
     
